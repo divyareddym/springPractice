23731f493e0cba8ff64b2856fc9c124d2bde2635
narenderreddym <narenderreddym@narenderreddy>
2020-10-07 13:58:19 +0530

    divya-done specialist updates

--- springDemo/src/com/springDemo/test/Specialist.java
+++ springDemo/src/com/springDemo/test/Specialist.java
@@ -7,6 +7,25 @@ import org.springframework.stereotype.Component;
 public class Specialist{
 	@Value("gp")
 	String degree;
+	Integer id;
+	Integer salary;
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
 
 	public String getDegree() {	
 		return degree;
